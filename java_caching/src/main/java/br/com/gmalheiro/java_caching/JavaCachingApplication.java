5c7f2c477bb4bcf1ceec72a069fa85cb96768383
Gabriel Malheiro <gsmalheiro@rd.com.br>
2025-01-19 22:35:14 -0300

    feat: add spring project

---
+++ java_caching/src/main/java/br/com/gmalheiro/java_caching/JavaCachingApplication.java
@@ -0,0 +1,13 @@
+package br.com.gmalheiro.java_caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCachingApplication.class, args);
+	}
+
+}
---
+++ java_caching/src/test/java/br/com/gmalheiro/java_caching/JavaCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gmalheiro.java_caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
