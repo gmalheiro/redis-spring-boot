01f6452f61238308edeae2c2609f2e35a82813ea
Gabriel Malheiro <gsmalheiro@rd.com.br>
2025-01-19 22:46:03 -0300

    feat: add approach without caching

--- java_caching/src/main/java/br/com/gmalheiro/java_caching/JavaCachingApplication.java
+++ java_caching/src/main/java/br/com/gmalheiro/java_caching/JavaCachingApplication.java
@@ -1,7 +1,9 @@
 package br.com.gmalheiro.java_caching;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JavaCachingApplication {
@@ -10,4 +12,15 @@ public class JavaCachingApplication {
 		SpringApplication.run(JavaCachingApplication.class, args);
 	}
 
+	@Bean
+	ApplicationRunner runner (ProductService productService) {
+		return args -> {
+			System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+			System.out.println("Id: 1: " + productService.getById(1L));
+			System.out.println("Id: 2: " + productService.getById(2L));
+			System.out.println("Id: 1: " + productService.getById(1L));
+			System.out.println("Id: 1: " + productService.getById(1L));
+			System.out.println("Id: 1: " + productService.getById(1L));
+		};
+	}
 }
---
+++ java_caching/src/main/java/br/com/gmalheiro/java_caching/entity/Product.java
@@ -0,0 +1,4 @@
+package br.com.gmalheiro.java_caching.entity;
+
+public record Product(Long id, String name, String description) {
+}
