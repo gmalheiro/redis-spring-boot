9ffa47151fff134c728d35f65d9beb145872785b
Gabriel Malheiro <gsmalheiro@rd.com.br>
2025-01-19 23:55:57 -0300

    refactor: record implements serializable

--- java_caching/src/main/java/br/com/gmalheiro/java_caching/entity/Product.java
+++ java_caching/src/main/java/br/com/gmalheiro/java_caching/entity/Product.java
@@ -1,4 +1,6 @@
 package br.com.gmalheiro.java_caching.entity;
 
-public record Product(Long id, String name, String description) {
+import java.io.Serializable;
+
+public record Product(Long id, String name, String description) implements Serializable {
 }
